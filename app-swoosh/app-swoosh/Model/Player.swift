1d02ce6d55636de8fb46d967b77cc264fc3b3b1e
armand chariot <armand@chariot.be>
2019-06-06 15:38:54 +0200

    learned to passe data from different wiew with prepare for segue, learn to create IBAction and IBoutlet, used also performSegue to point a destitantion view with a button

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Armand Chariot on 06/06/2019.
+//  Copyright © 2019 Armand Chariot. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
